5b244b7d338837a421ed5a852557b2462246cd5a
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-11 23:02:30 +0000

    axios fetch events api data within console

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./events.scss"
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 
 
 export default function Events() {
+
+  // function getEvents() {
+  //     axios.get("/api/events").then((res) => {console.log(res.data)})
+  // }
+
+
+
   return (
     <h1>HELLLOOO HOME</h1> 
+
   )
 }
