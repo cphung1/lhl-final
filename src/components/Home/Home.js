bf1aa7459d2f9c55e437490a4c500bd4cadd60e9
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-16 15:32:35 -0700

    removed commented out code in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export default function Home(props) {
   return (
     <div> 
+      <footer>
       <Link to="/upcoming_events">Upcoming Events</Link>
       <Link to="/my_events">My Events</Link>
-      <footer>
       </footer>
     </div>
   )
