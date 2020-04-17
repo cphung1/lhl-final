fd8844620a3f89d7ca5c815dcaf8c67e465c4ed9
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-17 00:06:47 -0700

    added class

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -5,16 +5,19 @@ import { Link } from 'react-router-dom'
 export default function Event(props) {
   return (
     <div className="ListEvents"> 
+      <div className="event-container">
+
       <h1>{props.name}</h1>
       <h3>{new Date(props.start_date).toDateString().slice(4)} - {new Date(props.end_date).toDateString().slice(4)}</h3>
       <h4>{props.location}</h4>
         <button onClick={() => props.setEvent(props.id)}>
           details
         </button>
-        
+
         <Link to="swipe">
           <button onClick={() => {props.getFilterUsers(props.user, props.id); props.fetchSingleEvent(props.id)}}>Start Swiping</button>
         </Link>
+      </div>
     </div>
   )
 }
