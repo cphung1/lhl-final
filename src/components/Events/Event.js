eebc5bd2c2739dd9c69cd9d1e11da7c315fb91ea
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-17 15:11:35 -0700

    event logos rendered

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import "./event.scss"
 import { Link } from 'react-router-dom'
+import Image from 'react-bootstrap/Image'
 
 export default function Event(props) {
   return (
     <div className="ListEvents"> 
       <div className="event-container">
+        <div className="content-wrapper">
+        <Image className="avatar" src={`./images/events/${props.id}.jpg`} roundedCircle/>
+          <div className="text-wrapper">
+            <h2>{props.name}</h2>
+            <h3>{new Date(props.start_date).toDateString().slice(4)} - {new Date(props.end_date).toDateString().slice(4)}</h3>
+            <h4>{props.location}</h4>
+              <button onClick={() => props.setEvent(props.id)}>
+                details
+              </button>
 
-      <h1>{props.name}</h1>
-      <h3>{new Date(props.start_date).toDateString().slice(4)} - {new Date(props.end_date).toDateString().slice(4)}</h3>
-      <h4>{props.location}</h4>
-        <button onClick={() => props.setEvent(props.id)}>
-          details
-        </button>
-
-        <Link to="swipe">
-          <button onClick={() => {props.getFilterUsers(props.user, props.id); props.fetchSingleEvent(props.id)}}>Start Swiping</button>
-        </Link>
+              <Link to="swipe">
+                <button onClick={() => {props.getFilterUsers(props.user, props.id); props.fetchSingleEvent(props.id)}}>Start Swiping</button>
+              </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
