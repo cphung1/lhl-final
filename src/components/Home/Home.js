fece3c10a38c652bb0e6297efb586e5552272a65
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-17 14:20:35 -0700

    removed img and spaced out buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,9 +6,6 @@ export default function Home(props) {
   return (
     <div> 
       <img src="/images/logo/LogoMakr_62F6RG.png"></img>
-      <img id="main-photo" src="/images/events/umf.jpg"></img>
-      <div className="main-photo">
-      </div>
       <div id="event-wrapper">
         <div className="event-wrapper">
           <Link to="/upcoming_events">Upcoming Events</Link>
