ffd138a3014b6d6036e678de43891afa3c1eb9f2
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-18 22:14:55 -0700

    styling login

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -21,20 +21,22 @@ export default function MyProfile(props) {
     <div className='myprofile'> 
       <h1>My Profile</h1>
       <div className="myProfileContent">
-      <Card>
+      <Card style={{ width: '35rem' }}>
         <Card.Body>
           <div className="imgdiv">
 
         <Card.Img className="cardImg" variant="top" src={`./images/users/${props.myProfile.id}.jpg`} alt={props.myProfile.name} />
           </div>
 
-        <Card.Title className="pofileName">{props.myProfile.name}, {props.myProfile.birthdate}</Card.Title>
-        <Card.Title>{props.myProfile.location}</Card.Title>
-        <Card.Text>{props.myProfile.description}</Card.Text>
+        <div className="profile-content-wrapper">
+          <Card.Title className="pofileName">{props.myProfile.name}, {props.myProfile.birthdate}</Card.Title>
+          <Card.Title>{props.myProfile.location}</Card.Title>
+          <Card.Text>{props.myProfile.description}</Card.Text>  
+        </div>
         </Card.Body>
       </Card>
       </div>
-      <button onClick={() => props.logout()}>logout</button>
+      <button onClick={() => props.logout()}>Logout</button>
     </div>
   )
 }
