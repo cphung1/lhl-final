94dba6d6583e605670c9423ce273249b304e1f81
cphung1 <cphung97@gmail.com>
2020-04-20 20:41:04 -0600

    added edit function to my profile description

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,17 +1,43 @@
-import React, {useEffect} from 'react'
+import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import "./MyProfile.scss"
-
+import classnames from "classnames";
+import axios from 'axios';
 
 export default function MyProfile(props) {
   const calculateAge = (birthdate) => {
-    const currentYear = new Date().getFullYear()
-    return currentYear - birthdate
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthdate;
   }
 
-  // useEffect(() => {
-  //   props.getMyProfileDetails(props.user)
-  // }, [])
+  const editProfile = () => {
+    
+    const data = {
+      description: descState.description
+    }
+
+    console.log(data)
+    return axios.patch(`/api/users/${props.myProfile.id}`, data)
+    .then((res) => {
+      console.log(res)
+    }).catch(error => 
+      console.log("Cannot edit profile")
+    )
+  }
+
+  const [edit, setEdit] = useState(true)
+  
+  const formClass = classnames('form', {'form-disabled' : edit})
+
+  const editBtnClass = classnames('editBtn', {'editBtn-disabled' : !edit})
+
+  const saveBtnClass = classnames('saveBtn-disabled', {'saveBtn' : !edit})
+
+  const [descState, setDesc] = useState({ description: props.myProfile.description });
+
+  const toggleEdit = (state) => setEdit(state)
+
+  const handleDesc = event => (setDesc({[event.target.name]: event.target.value}));
 
   if(!props.user){
     return null;
@@ -30,15 +56,20 @@ export default function MyProfile(props) {
 
         <div className="profile-content-wrapper">
           <Card.Title className="profile-font-name">{props.myProfile.name}, {calculateAge(props.myProfile.birthdate.slice(0, 4))}</Card.Title>
-          <Card.Title className="profile-font">{props.myProfile.location}</Card.Title>
-          <Card.Text className="profile-font">{props.myProfile.description}</Card.Text>  
+          <Card.Title className="profile-font">{props.myProfile.location} </Card.Title>
+          <Card.Text className="profile-font">
+            <form>
+              <textarea className={formClass} tyoe="text" name="description" value={descState.description} onChange={handleDesc} disabled={edit}/>
+            </form>
+          </Card.Text>  
         </div>
         </Card.Body>
       </Card>
       </div>
-      <button onClick={() => props.logout()}>Logout</button>
+      <button className={editBtnClass} onClick={() => toggleEdit(false)}>Edit</button>
+      <button className={saveBtnClass} onClick={() => {toggleEdit(true); editProfile()}}>Save</button>
+      
+      <button className="logoutBtn" onClick={() => props.logout()}>Logout</button>
     </div>
   )
 }
-// calculateAge(props.myProfile.birthdate.slice(0, 4))
-// new Date(props.myProfile.birthdate).toDateString().slice(4)
